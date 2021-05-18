10ccff85a8b0aa3795de2176b8827de19828f7ff
sirkpp <krishnapd133@gmail.com>
2021-05-18 11:10:56 +0545

    few changes

--- mandelbrot.js
+++ mandelbrot.js
@@ -6,7 +6,11 @@ canvas.height = window.innerHeight;
 H = window.innerHeight;
 W = window.innerWidth;
 
-
+if(H > W){
+    H = H+W;
+    W = H - W;
+    H = H - W;
+}
 
 function scalePoint(point){
     x=point[0]/W, y=point[1]/H;
