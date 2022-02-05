ca61e79227d809483601e34bb0f9410fef63dc7d
sirkpp <krishnapd133@gmail.com>
2022-02-05 17:27:54 +0545

    fixed link issue

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let date = new Date();
 document.getElementById('copyright').innerHTML = '\u00A9' + date.getFullYear().toString() + ', Krishna Panthi, All Rights Reserved';
 
 function goToHome(e) {
-    window.location.href = 'https:\\krishnapanthi.com.np'
+    window.location.href = 'https://krishnapanthi.com.np'
 }
