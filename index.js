bca7cb6dc0985e291e008c0e5586ce5fa42b63aa
Krishna Panthi <krishnapd133@gmail.com>
2023-10-23 22:56:14 +0545

    cv update

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let date = new Date();
-document.getElementById('copyright').innerHTML = '\u00A9' + date.getFullYear().toString() + ', Krishna Panthi, All Rights Reserved';
+document.addEventListener("DOMContentLoaded", (event) => {
+  let date = new Date();
+  document.getElementById("copyright").innerHTML =
+    "\u00A9" +
+    date.getFullYear().toString() +
+    ", Krishna Panthi, All Rights Reserved";
 
-function goToHome(e) {
-    window.location.href = '/'
-}
+  function goToHome(e) {
+    window.location.href = "/";
+  }
+
+  let ageEl = document.getElementById('age');
+  ageEl.innerText = Math.round((new Date() - new Date('1997-08-25'))/(1000*60*60*24));
+});
