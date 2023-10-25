ec708c6ebb5b7fb05bb8fcffe3472757423442d1
Krishna Panthi <krishnapd133@gmail.com>
2023-10-25 21:31:41 +0545

    game version 1

--- Finance/finance.js
+++ Finance/finance.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         break;
 
       case allCalculations["SF"]:
-        result = Math.round(A * (I / (common - I)));
+        result = Math.round(A * (I / (common - 1)));
         break;
 
       case allCalculations["CR"]:
@@ -53,19 +53,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
     desc = "";
     switch (toCalculate) {
       case allCalculations["USCA"]:
-        desc = "Calculates the total value after given years, compounded annually."
+        desc = "Uniform Series Compound Interest refers to a series of equal cash flows, spaced equally in time and compounded at a certain interest rate. It’s commonly used in savings and loan repayment scenarios. The future value of a series of cash flows is calculated, taking into account the effect of compounding interest over the period. Here, amount means equal annual cash flow."
         break;
 
       case allCalculations["USPW"]:
-        desc = ""
+        desc = "Uniform Series Present Worth concept is used to determine the present value of a series of equal future cash flows, discounted at a certain interest rate. This is particularly useful for investment analysis, where you want to know the current worth of future earnings or costs, considering the time value of money. Here, amount means equal annual cash flows."
         break;
 
       case allCalculations["SF"]:
-        desc = ""
+        desc = "A sinking fund is a fund established to accumulate money over time to pay off a future expense or debt. Equal annual (or other periodic) deposits are made into the fund, which earns interest. The purpose is to spread out the cost of a large future expense over multiple periods, making it more manageable. Here, amount means future worth."
         break;
 
       case allCalculations["CR"]:
-        desc = ""
+        desc = "Capital recovery involves determining the annual equivalent amount required to recoup an initial investment over a specified period, considering the time value of money. This is often used in the context of asset depreciation and loan repayments, to establish a schedule of payments that will cover the initial cost plus interest. Here, amount means present worth."
         break;
 
       default:
--- Games/Blend/blend.js
+++ Games/Blend/blend.js
@@ -1,91 +1,205 @@
-let selectedColors = [];
+let selectedColors = {
+  "rgb(255, 0, 0)": 0, // Red
+  "rgb(0, 255, 0)": 0, // Green
+  "rgb(0, 0, 255)": 0, // Blue
+  "rgb(255, 255, 0)": 0, // Yellow
+  "rgb(0, 255, 255)": 0, // Cyan
+  "rgb(255, 0, 255)": 0, // Magenta
+  "rgb(0, 0, 0)": 0, // White
+  "rgb(255, 255, 255)": 0, // Black
+  "rgb(128, 128, 128)": 0, // Gray
+  "rgb(255, 165, 0)": 0, // Orange
+};
+
+var targetColor = "rgb(128, 169, 128)";
+
 let count = 0;
 
 document.addEventListener("DOMContentLoaded", (event) => {
   const colors = [
-    "#FF0000",
-    "#00FF00",
-    "#0000FF",
-    "#FFFF00",
-    "#00FFFF",
-    "#FF00FF",
-    "#000000",
-    "#FFFFFF",
-    "#808080",
-    "#FFA500",
+    "rgb(255, 0, 0)", // Red
+    "rgb(0, 255, 0)", // Green
+    "rgb(0, 0, 255)", // Blue
+    "rgb(255, 255, 0)", // Yellow
+    "rgb(0, 255, 255)", // Cyan
+    "rgb(255, 0, 255)", // Magenta
+    "rgb(0, 0, 0)", // White
+    "rgb(255, 255, 255)", // Black
+    "rgb(128, 128, 128)", // Gray
+    "rgb(255, 165, 0)", // Orange
   ];
+
+  var targetEl = document.getElementById("target");
+  if (targetEl) {
+    targetEl.style.backgroundColor = targetColor;
+  }
+
   colors.forEach((color, index) => {
     const div = document.getElementById(`color-${index + 1}`);
     if (div) {
       div.style.backgroundColor = color;
-      if (color === "#000000" || color === "#0000FF" || color === "#808080") {
+      if (
+        color === "rgb(0, 0, 0)" ||
+        color === "rgb(0, 0, 255)" ||
+        color === "rgb(128, 128, 128)"
+      ) {
         div.style.color = "#FFFFFF"; // Change text color to white for better contrast
       }
     }
   });
 
+  const colorRemoveBlocks = document.getElementsByClassName("remove-color");
+
+  Array.from(colorRemoveBlocks).forEach((block) => {
+    block.addEventListener("click", function () {
+      let parent = block.parentElement;
+      let colorEl = parent.getElementsByClassName("select-color")[0];
+      let colorToBeRemoved = getComputedStyle(colorEl).backgroundColor;
+      if (selectedColors[colorToBeRemoved]) {
+        selectedColors[colorToBeRemoved] -= 1;
+        updateColor();
+        updatePercentage();
+      }
+    });
+  });
+
   const colorBlocks = document.getElementsByClassName("select-color");
+
   Array.from(colorBlocks).forEach((block) => {
     block.addEventListener("click", function () {
-        if(count === 10){
-            alert("You Lost!")
-            return;
-        }
-      let selectedColor = getComputedStyle(this).backgroundColor;
-      selectedColors.push(selectedColor);
+      block.innerText = "33%";
 
-      console.log(`Background color: ${selectedColor}`);
+      let selectedColor = getComputedStyle(this).backgroundColor;
+      if (selectedColor in selectedColors) {
+        selectedColors[selectedColor] += 1;
+      }
+
       this.classList.add("clicked");
 
       setTimeout(() => {
         this.classList.remove("clicked");
       }, 300); // The timeout duration should match the animation duration
 
-      const elem = document.getElementById("your-color");
-      if (elem) {
-        elem.style.backgroundColor = averageRGBColors(selectedColors);
-      }
+      updateColor();
       count++;
+      setCount();
+      updatePercentage();
 
-      const countEl = document.getElementById("count");
-      countEl.innerText = count;
+      setTimeout(() => seeIfGameIsDone(), 400);
+      
     });
   });
+
+  let resetBtn = document.getElementById("reset");
+
+  resetBtn.onclick = function () {
+    resetYourColor();
+    resetColors();
+    count = 0;
+    setCount();
+    updatePercentage();
+  };
+
+  setCount();
+
+  function seeIfGameIsDone(){
+    if (checkIfColorsMatched()) {
+      let x = alert("Congrats! You won.");
+      console.log("you won! --> ", x);
+    } else if (count === 10) {
+      alert("You Lost!");
+      return;
+    }
+  }
 
   function averageRGBColors(colors) {
     let totalR = 0,
       totalG = 0,
       totalB = 0;
+    var len = 0;
 
-    colors.forEach((color) => {
+    for (let color in colors) {
+      let val = colors[color];
+      len += val;
       const rgb = color.match(/\d+/g).map(Number);
-      totalR += rgb[0];
-      totalG += rgb[1];
-      totalB += rgb[2];
-    });
+      totalR += rgb[0] * val;
+      totalG += rgb[1] * val;
+      totalB += rgb[2] * val;
+    }
 
-    const avgR = Math.round(totalR / colors.length);
-    const avgG = Math.round(totalG / colors.length);
-    const avgB = Math.round(totalB / colors.length);
+    if (len === 0) {
+      resetYourColor();
+      return "";
+    }
+
+    const avgR = Math.round(totalR / len);
+    const avgG = Math.round(totalG / len);
+    const avgB = Math.round(totalB / len);
 
     return `rgb(${avgR}, ${avgG}, ${avgB})`;
   }
 
-  let resetBtn = document.getElementById("reset");
-  resetBtn.onclick = function () {
-    selectedColors = [];
+  function resetYourColor() {
     const body = document.getElementsByTagName("body");
     var resetColor = getComputedStyle(body[0]).backgroundColor;
     const elem = document.getElementById("your-color");
     if (elem) {
       elem.style.backgroundColor = resetColor;
     }
+  }
 
-    count = 0;
+  function resetColors() {
+    selectedColors = {
+      "rgb(255, 0, 0)": 0, // Red
+      "rgb(0, 255, 0)": 0, // Green
+      "rgb(0, 0, 255)": 0, // Blue
+      "rgb(255, 255, 0)": 0, // Yellow
+      "rgb(0, 255, 255)": 0, // Cyan
+      "rgb(255, 0, 255)": 0, // Magenta
+      "rgb(0, 0, 0)": 0, // White
+      "rgb(255, 255, 255)": 0, // Black
+      "rgb(128, 128, 128)": 0, // Gray
+      "rgb(255, 165, 0)": 0, // Orange
+    };
+  }
+
+  function setCount() {
     const countEl = document.getElementById("count");
     countEl.innerText = count;
-  };
+  }
 
-  const countEl = document.getElementById("count");
-  countEl.innerText = count;
+  function updateColor() {
+    const elem = document.getElementById("your-color");
+    if (elem) {
+      let avgColor = averageRGBColors(selectedColors);
+      if (avgColor) {
+        elem.style.backgroundColor = avgColor;
+      }
+    }
+  }
+
+  function updatePercentage() {
+    const colorBlocks = document.getElementsByClassName("select-color");
+    var totalColors = 0;
+    for (let color in selectedColors) {
+      totalColors += selectedColors[color];
+    }
+    Array.from(colorBlocks).forEach((block) => {
+      let selectedColor = getComputedStyle(block).backgroundColor;
+      let val = Math.round(100 * (selectedColors[selectedColor] / totalColors));
+      if (val) {
+        block.innerText = `${val}%`;
+      } else {
+        block.innerText = "";
+      }
+    });
+  }
+
+  function checkIfColorsMatched() {
+    var finalColor = averageRGBColors(selectedColors);
+    if (finalColor == targetColor) {
+      return true;
+    }
+    return false;
+  }
 });
